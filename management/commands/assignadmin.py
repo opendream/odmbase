75ba367dfcd90234ce5f76f6d431e4e6a24e431f
crosalot <crosalot#gmail.com>
2015-02-24 11:09:04 +0700

    python manage.py assignadmin admin@base.com

---
+++ management/commands/assignadmin.py
@@ -0,0 +1,20 @@
+from django.contrib.auth import get_user_model
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+
+    help = 'Assign admin by email for exist user in system'
+
+    def handle(self, *args, **options):
+
+        User = get_user_model()
+
+        for email in args:
+            try:
+                user = User.objects.get(email=email)
+                user.is_staff = True
+                user.is_superuser = True
+                user.save()
+            except User.DoesNotExist:
+                print '%s does not exist' % email
